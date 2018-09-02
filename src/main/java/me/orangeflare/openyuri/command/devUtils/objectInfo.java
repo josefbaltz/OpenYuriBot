3dc89e310d2df922c28330e7d8605c17b77272dc
Josef Baltz <josef@baltz.net>
2018-09-01 22:32:48 -0500

    Finished ChannelInfo

--- src/main/java/me/orangeflare/openyuri/command/devUtils/objectInfo.java
+++ src/main/java/me/orangeflare/openyuri/command/devUtils/objectInfo.java
@@ -2,35 +2,41 @@ package me.orangeflare.openyuri.command.devUtils;
 
 import org.javacord.api.entity.Nameable;
 import org.javacord.api.entity.channel.ServerChannel;
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.awt.*;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
 import static me.orangeflare.openyuri.Main.commandIssued;
+import static me.orangeflare.openyuri.Main.getResource;
 
 public class objectInfo implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
         String formattedContent = event.getMessage().getContent().toLowerCase();
 
-        //TODO Optimize this Code
         if(formattedContent.startsWith("y.channelinfo")) {
             commandIssued(event, "channelInfo");
-            /*
-            new MessageBuilder()
-                    .append(event.getMessage().getChannel().asServerChannel().map(ServerChannel::getName).get() + "\n")
-                    .append(event.getMessage().getChannel().asServerChannel().map(ServerChannel::getIdAsString).get())
-                    .send(event.getChannel());
-            */
+
+            TextChannel channel = event.getMessage().getChannel();
             new MessageBuilder()
                     .setEmbed(new EmbedBuilder()
-                            .setAuthor("OpenYuri"))
-
+                            .setAuthor("OpenYuri")
+                            .setThumbnail(getResource("/about/thumbnail.png"))
+                            .setTitle("Channel Information")
+                            .addField("Name", channel.asServerChannel().map(ServerChannel::getName).get(), true)
+                            .addField("ID", channel.asServerChannel().map(ServerChannel::getIdAsString).get(), true)
+                            .addField("Category", channel.asServerTextChannel().map(ServerTextChannel::getCategory).get().map(Nameable::getName).get(), true)
+                            .setColor(Color.decode("#9c27b0"))
+                    )
+                    .send(event.getChannel());
         }
 
         if(formattedContent.startsWith("y.emojilist") || formattedContent.startsWith("y.emojis")) {
